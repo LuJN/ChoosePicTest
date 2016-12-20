8148dedc99e6623d8db2467348c02f4d934ff827
XingYunLiuShui <2290617605@qq.com>
2016-12-20 18:32:11 +0800

    First Commit

---
+++ app/src/main/java/com/example/choosepictest/MainActivity.java
@@ -0,0 +1,185 @@
+package com.example.choosepictest;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final int TAKE_PHOTO = 1;
+    private static final int CROP_PHOTO = 2;
+    private static final int CHOOSE_PHOTO = 3;
+    private Button mTakePhoto;
+    private Button mChooseFromAlbum;
+    private ImageView mPicture;
+    private Uri mImageUri;
+    private static final int REQUEST_EXTERNAL_STORAGE = 100;
+    private static String[] PERMISSIONS_EXTERNAL_STORAGE = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        verifyStoragePermissions(this);
+        mTakePhoto = (Button) findViewById(R.id.id_take_photo);
+        mChooseFromAlbum = (Button) findViewById(R.id.id_choose_from_album);
+        mPicture = (ImageView) findViewById(R.id.id_picture);
+        mTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File outputImage = new File(Environment.getExternalStorageDirectory(),
+                        "output_image.jpg");
+                if(outputImage.exists()){
+                    outputImage.delete();
+                }
+                try {
+                    outputImage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mImageUri = Uri.fromFile(outputImage);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+            }
+        });
+        mChooseFromAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, CHOOSE_PHOTO);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_OK) {
+            return;
+        }
+        switch(requestCode) {
+            case TAKE_PHOTO:
+                int a = 0;
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageUri, "image/*");
+                intent.putExtra("scale", true);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+                startActivityForResult(intent, CROP_PHOTO);
+                break;
+            case CROP_PHOTO:
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeStream(
+                            getContentResolver().openInputStream(mImageUri));
+                    if(bitmap == null) {
+                        Log.i(TAG, "bitmap == null");
+                    }
+                    mPicture.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case CHOOSE_PHOTO:
+                // 判断手机系统版本号
+                if(Build.VERSION.SDK_INT >= 19) {
+                    // 4.4及以上系统使用这个方法处理图片
+                    handleImageOnKitKat(data);
+                } else {
+                    // 4.4以下系统使用这个方法处理图片
+                    handleImageBeforeKitKat(data);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if(DocumentsContract.isDocumentUri(this, uri)) {
+            // 如果是document类型的Uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if(uri.getAuthority().equals("com.android.providers.media.documents")) {
+                String id = docId.split(":")[1];// 解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + " = " + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if(uri.getAuthority().equals("com.android.providers.downloads.documents")) {
+                Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+        } else if(uri.getScheme().equalsIgnoreCase("content")) {
+            // 如果不是document类型的Uri，则使用普通方式处理
+            imagePath = getImagePath(uri, null);
+        }
+        displayImage(imagePath);// 根据图片路径显示图片
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        // 通过Uri和selection来获取真实的图片路径
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if(cursor != null) {
+            if(cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void displayImage(String imagePath) {
+        if(imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            mPicture.setImageBitmap(bitmap);
+        } else {
+            Toast.makeText(MainActivity.this, "failed to get image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permissionWrite = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionWrite != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_EXTERNAL_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
+    }
+}
